2f3af95871e518ec8496c6fb46be968d8f528b56
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2022-12-22 20:18:16 -0300

    feat: update productsData and container styles

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,72 +1,76 @@
-const buttonsList = document.querySelector('.buttons-list');
-const rangeBar = document.querySelector('#range-bar');
+const buttonsList = document.querySelector(".buttons-list");
+const rangeBar = document.querySelector("#range-bar");
 
 renderCategories();
 renderAlbums(products);
 
 /* ----------- INSERIR DINAMICAMENTE BOTÕEs DO FILTRO ------------- */
 function renderCategories() {
-    let count = 0;
-    categories.forEach(category => {
-        buttonsList.insertAdjacentHTML('beforeend',
-            `<li>
+  let count = 0;
+  categories.forEach((category) => {
+    buttonsList.insertAdjacentHTML(
+      "beforeend",
+      `<li>
                 <button data-category="${count}">${category}</button>
             </li>`
-        )
-        count++
-    })
-    filterCategory(products);
+    );
+    count++;
+  });
+  filterCategory(products);
 }
-
 
 /* ------------ MAPEAR BOTÕES DO FILTRO ----------- */
 function filterCategory(array) {
-    let category = 0;
-    const categoriesList = document.querySelectorAll('[data-category]');
-    const maxPrice = document.querySelector('.max-price span')
-    categoriesList[0].classList.add('button-selected')
+  let category = 0;
+  const categoriesList = document.querySelectorAll("[data-category]");
+  const maxPrice = document.querySelector(".max-price span");
+  categoriesList[0].classList.add("button-selected");
 
-    rangeBar.oninput = (event) => {
-        let target = event.target
-        const min = target.min
-        const max = target.max
-        const value = Number(target.value)
+  rangeBar.oninput = (event) => {
+    let target = event.target;
+    const min = target.min;
+    const max = target.max;
+    const value = Number(target.value);
 
-        target.style.backgroundSize = (value - min) * 100 / (max - min) + '% 100%'
-        maxPrice.innerText = `Até R$ ${value.toFixed(2).replace('.', ',')}`
+    target.style.backgroundSize =
+      ((value - min) * 100) / (max - min) + "% 100%";
+    maxPrice.innerText = `Até R$ ${value.toFixed(2).replace(".", ",")}`;
 
-        filterItems(array,category)
-    }
+    filterItems(array, category);
+  };
 
-    categoriesList.forEach(element => {
-        element.onclick = () => {
-            categoriesList.forEach(element => element.classList.remove('button-selected'))
-            category = element.getAttribute('data-category')
-            element.classList.add('button-selected')
+  categoriesList.forEach((element) => {
+    element.onclick = () => {
+      categoriesList.forEach((element) =>
+        element.classList.remove("button-selected")
+      );
+      category = element.getAttribute("data-category");
+      element.classList.add("button-selected");
 
-            filterItems(array,category)
-        }
-    })
+      filterItems(array, category);
+    };
+  });
 }
-
 
 /* ------------- FILTRAR OS DADOS RECEBIDOS --------------*/
-function filterItems(array,category) {
-    const value = Number(rangeBar.value)
-    const filteredItems = array.filter(element => {
-        return (element.category == category || category == 0) && element.price <= value
-    })
-    renderAlbums(filteredItems);
+function filterItems(array, category) {
+  const value = Number(rangeBar.value);
+  const filteredItems = array.filter((element) => {
+    return (
+      (element.category == category || category == 0) && element.price <= value
+    );
+  });
+  renderAlbums(filteredItems);
 }
-
 
 /* -------------- RENDERIZAR OS DADOS RECEBIDOS --------------*/
 function renderAlbums(array) {
-    const albumList = document.querySelector('.album-list');
-    albumList.innerHTML = ''
-    array.forEach(element => {
-        albumList.insertAdjacentHTML('afterbegin',
-            `<li class="album-item">
+  const albumList = document.querySelector(".album-list");
+  albumList.innerHTML = "";
+  array.forEach((element) => {
+    albumList.insertAdjacentHTML(
+      "afterbegin",
+      `<li class="album-item">
             <div class="album-image">
             <img src=${element.img} alt="album musical">
             </div>
@@ -77,11 +81,11 @@ function renderAlbums(array) {
             </div>
             <h3>${element.title}</h3>
             <div>
-                <span>R$ ${element.price.toFixed(2).replace('.', ',')}</span>
+                <span>R$ ${element.price.toFixed(2).replace(".", ",")}</span>
                 <button>Comprar</button>
             </div>
             </div>
         </li>`
-        )
-    });
+    );
+  });
 }
